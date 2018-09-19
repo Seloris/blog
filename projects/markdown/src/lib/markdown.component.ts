8abf1bb0de63df28132317bbc86a0e9ebee4372c
Daniel Djordjevic <ddjordjevic@activeassurances.fr>
2018-09-19 13:49:19 +0200

    init angular + md project

---
+++ projects/markdown/src/lib/markdown.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'md-markdown',
+  templateUrl: './markdown.component.html',
+  styles: []
+})
+export class MarkdownComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+
+}
---
+++ projects/markdown/src/lib/markdown.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { MarkdownComponent } from './markdown.component';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [MarkdownComponent],
+  exports: [MarkdownComponent]
+})
+export class MarkdownModule { }
