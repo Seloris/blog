1969fd8c46589906c9093a58d6c4e9b905bf80aa
Seloris <daniel.djordjevic@viacesi.fr>
2018-10-18 09:34:31 +0200

    Test typo + start card

--- Blog.Api/Blog.Front/src/app/typography.ts
+++ Blog.Api/Blog.Front/src/app/typography.ts
@@ -1,10 +1,6 @@
 import Typography from 'typography';
 
-const typography = new Typography({
-  baseFontSize: '18px',
-  baseLineHeight: 1.666,
-  headerFontFamily: ['Avenir Next', 'Helvetica Neue', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif'],
-  bodyFontFamily: ['Georgia', 'serif']
-  // See below for the full list of options.
-});
+import fairyGates from 'typography-theme-fairy-gates';
+
+const typography = new Typography(fairyGates);
 typography.injectStyles();
